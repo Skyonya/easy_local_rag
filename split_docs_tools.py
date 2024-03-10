4e165d6c489ac13212623e179eb59a826931c6f1
Skyonya <strokanevks@ya.ru>
2024-03-10 16:28:55 +0400

    fix: add logs

--- split_docs_tools.py
+++ split_docs_tools.py
@@ -2,7 +2,7 @@ from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import CharacterTextSplitter
 
 
-def split_document(documents_path, chunk_size=1000, chunk_overlap=0):
+def split_document(documents_path, chunk_size=1000, chunk_overlap=0, silent=True):
     # load the document
     loader = TextLoader(documents_path)
     documents = loader.load()
@@ -10,4 +10,6 @@ def split_document(documents_path, chunk_size=1000, chunk_overlap=0):
     # split it into chunks
     text_splitter = CharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     docs = text_splitter.split_documents(documents)
+    if not silent:
+        print(f'Created {len(docs)} splits from {documents_path}')
     return docs
