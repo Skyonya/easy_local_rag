889a31982c80a2a2ff079ce800460956a6d112de
Skyonya <strokanevks@ya.ru>
2024-03-10 18:37:52 +0400

    fix: update with create_vectorstore and load_vectorstore

--- start_model.py
+++ start_model.py
@@ -1,26 +1,36 @@
 from split_docs_tools import split_document
-from vectorstore_tools import vectorstore_process
-from model_answer import  get_model_answer
+from vectorstore_tools import load_vectorstore, create_vectorstore
+from model_answer import get_model_answer
+from tools import is_directory_empty
+
+documents_path = 'documents/hp1.txt'
+persist_directory = 'vectorstore_db'
 
 if __name__ == "__main__":
 
-    # split documents
-    docs = split_document(
-        documents_path='documents/hp1.txt',
-        chunk_size=2000,
-        chunk_overlap=100,
-        silent=False
-    )
+    if is_directory_empty(documents_path):
 
-    # =============================================
+        # split documents
+        docs = split_document(
+            documents_path=documents_path,
+            chunk_size=2000,
+            chunk_overlap=100,
+            silent=False
+        )
 
-    # create embeddings and save to Chroma vectorstore_db
-    db = vectorstore_process(
-        docs=docs,
-        embedding_model_name="all-MiniLM-L6-v2",
-        persist_directory='vectorstore_db',
-        silent=False
-    )
+        db = create_vectorstore(
+            docs=docs,
+            embedding_model_name="all-MiniLM-L6-v2",
+            persist_directory='vectorstore_db',
+            silent=False
+        )
+
+    else:
+        db = load_vectorstore(
+            embedding_model_name="all-MiniLM-L6-v2",
+            persist_directory='vectorstore_db',
+            silent=False
+        )
 
     # =============================================
 
