3639c53abd1df002ad0534d9fb78edf2e9cef0f0
Skyonya <strokanevks@ya.ru>
2024-03-23 21:00:20 +0400

    feat: add get_vectorstore that can create and load vector db

--- vectorstore_tools.py
+++ vectorstore_tools.py
@@ -1,7 +1,11 @@
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 
+from split_docs_tools import split_document
+from tools import is_directory_empty, decorator_timer
 
+
+@decorator_timer
 def create_vectorstore(
         docs,
         embedding_model_name,
@@ -16,10 +20,11 @@ def create_vectorstore(
     db = Chroma.from_documents(documents=docs, embedding=embedding_function, persist_directory=persist_directory)
     db.persist()
     if not silent:
-        print(f'Vector store created and saved to {persist_directory}')
+        print(f'Vector store created and saved to folder: {persist_directory}')
     return db
 
 
+@decorator_timer
 def load_vectorstore(
         embedding_model_name,
         persist_directory,
@@ -32,4 +37,44 @@ def load_vectorstore(
     db = Chroma(embedding_function=embedding_function, persist_directory=persist_directory)
     if not silent:
         print(f'Vector store loaded')
+    return db
+
+
+def get_vectorstore(
+        documents_path,
+        embedding_model_name,
+        persist_directory,
+        chunk_size=2000,
+        chunk_overlap=100,
+        silent=True
+):
+    if is_directory_empty(persist_directory, silent=silent):
+
+        # split documents
+        docs = split_document(
+            documents_path=documents_path,
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+            silent=silent
+        )
+
+        # create vector store db
+        db, time = create_vectorstore(
+            docs=docs,
+            embedding_model_name=embedding_model_name,
+            persist_directory=persist_directory,
+            silent=silent
+        )
+        if not silent:
+            print(f'Creating time', time)
+
+    else:
+        # load vector store db
+        db, time = load_vectorstore(
+            embedding_model_name=embedding_model_name,
+            persist_directory=persist_directory,
+            silent=silent
+        )
+        if not silent:
+            print(f'Loading time', time)
     return db
