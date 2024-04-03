3d10f1182064f4df201ff23b67400e173aec5b8f
Skyonya <strokanevks@ya.ru>
2024-04-03 20:54:51 +0400

    feat: add logging split_docs_tools.py

--- split_docs_tools.py
+++ split_docs_tools.py
@@ -2,11 +2,14 @@ from langchain_community.document_loaders import TextLoader
 # from langchain_text_splitters import CharacterTextSplitter
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+import logging
+
 
 def load_document(documents_path):
     # load the document
     loader = TextLoader(documents_path)
     documents = loader.load()
+    logging.info('Documents was loaded')
     return documents
 
 
@@ -19,6 +22,7 @@ def split_document(
     # text_splitter = CharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     docs = text_splitter.split_documents(documents)
+    logging.info('Documents was splitted')
     return docs
 
 
@@ -26,10 +30,8 @@ def load_and_split_docs(
         documents_path,
         chunk_size=1000,
         chunk_overlap=100,
-        silent=True
 ):
     documents = load_document(documents_path)
     docs = split_document(documents, chunk_size, chunk_overlap)
-    if not silent:
-        print(f'Created {len(docs)} splits from {documents_path}')
+    logging.info(f'Created {len(docs)} splits from {documents_path}')
     return docs
